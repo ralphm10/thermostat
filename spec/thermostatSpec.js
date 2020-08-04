31d688ceb5336e0d9019e541f17a314a4a05110b
Ralph Mallett <rmallett12@gmail.com>
2020-08-04 17:23:07 +0100

    Update test spacing

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,32 +7,27 @@ describe("Thermostat", function() {
   });
 
   describe('starts at 20', function() {
-  it('should start with temperature of 20', function() {
-    expect(thermostat['temperature']).toEqual(20);
+    it('should start with temperature of 20', function() {
+      expect(thermostat['temperature']).toEqual(20);
+    });
   });
-});
 
   describe('increase temperature', function() {
     it('can increase the temperature', function() {
       thermostat.increase(5);
       expect(thermostat['temperature']).toEqual(25);
-
     });
-
   });
 
   describe('decrease temperature', function() {
     it('can decrease the temperature', function() {
       thermostat.decrease(5);
       expect(thermostat['temperature']).toEqual(15);
-
     });
-
   });
 
   describe('throws an error', function() {
-    it('throws error if user tries to reduce temp to below Minimum', function() {
-
+    it('throws error if user tries to reduce temp to below minimum', function() {
       expect(function() {thermostat.decrease(11)}).toThrowError("Minimum temperature: 10");
     });
   });
