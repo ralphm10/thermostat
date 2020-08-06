1bd601a0e9e1280f3f06debc5b665ec6c358e81e
Ralph Mallett <rmallett12@gmail.com>
2020-08-06 16:36:52 +0100

    Add jQuery for power saving on/off

--- interface.js
+++ interface.js
@@ -2,9 +2,11 @@ $( document ).ready(function() {
 
     thermostat = new Thermostat();
 
-    alert( "welcome to your thermostat" );
+
+    $('#power-saving-staus').text(thermostat.isPowerSaving())
 
     $('#temperature').text(thermostat.getTemperature())
+
 
     $('#increase-temp').click(function() { thermostat.increase()
       $('#temperature').text(thermostat.getTemperature() );
@@ -13,5 +15,15 @@ $( document ).ready(function() {
       $('#decrease-temp').click(function() { thermostat.decrease()
         $('#temperature').text(thermostat.getTemperature() );
     })
+
+    $('#reset-temp').click(function() { thermostat.reset()
+      $('#temperature').text(thermostat.getTemperature() );
+  })
+
+    $('#powersave-off').click(function() { thermostat.turnOffPowerSaving()
+    });
+
+    $('#powersave-on').click(function() { thermostat.turnOnPowerSaving()
+    });
 
 });
