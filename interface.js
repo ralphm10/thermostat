7c48297397c10b3381492fad85dc1cd830a82e13
Ralph Mallett <rmallett12@gmail.com>
2020-08-06 16:15:41 +0100

    Add jQuery for temperature, increase & decrease

--- interface.js
+++ interface.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
 
+    thermostat = new Thermostat();
+
     alert( "welcome to your thermostat" );
+
+    $('#temperature').text(thermostat.getTemperature())
+
+    $('#increase-temp').click(function() { thermostat.increase()
+      $('#temperature').text(thermostat.getTemperature() );
+    })
+
+      $('#decrease-temp').click(function() { thermostat.decrease()
+        $('#temperature').text(thermostat.getTemperature() );
+    })
 
 });
